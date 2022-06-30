42f3abd9f9e743e3aab9c355d15adf834aa55583
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-06-30 14:09:24 -0700

    work

--- switch-to-wifi-client.sh
+++ switch-to-wifi-client.sh
@@ -1,3 +1,3 @@
-node -c "require('./scripts/switch-to-wifi-client.js').createWPAConf()"
+node -e "require('./scripts/switch-to-wifi-client.js').createWPAConf()"
 sudo systemctl stop hostapd.service
 sudo systemctl stop dnsmasq.service
