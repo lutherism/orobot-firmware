80839647133cddb5f27a67ccf30f07658867664a
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-06-30 16:41:00 -0700

    work

--- switch-to-wifi-client.sh
+++ switch-to-wifi-client.sh
@@ -1,3 +1,8 @@
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 /home/pi/.config/nvm/versions/node/v8.17.0/bin/node -e "require('./scripts/switch-to-wifi-client.js').createWPAConf()"
 sudo systemctl stop hostapd.service
 sudo systemctl stop dnsmasq.service
