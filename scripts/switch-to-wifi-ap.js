268ea61bdab5b195b4d45c236537c4890a299d66
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-06-30 14:05:32 -0700

    work

--- scripts/switch-to-wifi-ap.js
+++ scripts/switch-to-wifi-ap.js
@@ -1,7 +1,29 @@
 const {spawn} = require('child_process');
+const fs = require('fs');
+
+const currentData = JSON.parse(
+  fs.readFileSync(__dirname + '/openroboticsdata/data.json')
+);
+
+const wpaConfPath = "/etc/wpa_supplicant/wpa_supplicant.conf";
+
+const createWPAConf = ({
+  ssid, psk
+}) => {
+  return `ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+update_config=1
+
+network={
+    ssid="OROBOT-Setup-${currentData.deviceUuid.slice(0, 5)}"
+    mode=2
+    key_mgmt=WPA-PSK
+    psk="wifisetup"
+    frequency=2412
+}`;
+}
 
 const upWifiAP = () => {
-  spawn(__dirname + '/../switch-to-wifi-ap.sh');
+  fs.writeFileSync(wpaConfPath, createWPAConf());
 }
 
 module.exports = {
--- switch-to-wifi-ap.sh
+++ switch-to-wifi-ap.sh
@@ -1,3 +1,4 @@
+node -c "require('./scripts/switch-to-wifi-ap.js').upWifiAP()"
 sudo ip link set dev wlan0 down
 sudo ip addr add 192.168.0.172/24 dev wlan0
 sudo systemctl restart dnsmasq.service
