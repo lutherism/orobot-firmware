5a05806b0b62f5953c2b85f1466cea720f3ba233
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-06-30 17:46:43 -0700

    work

--- switch-to-wifi-client.sh
+++ switch-to-wifi-client.sh
@@ -1,4 +1,6 @@
 /home/pi/.config/nvm/versions/node/v8.17.0/bin/node -e "require('./scripts/switch-to-wifi-client.js').writeWPAConf()"
 sudo systemctl stop hostapd.service
 sudo systemctl stop dnsmasq.service
-sudo ip link set dev wlan0 up
+sudo rfkill unblock all
+sudo ifconfig wlan0 down
+sudo ifconfig wlan0 up
