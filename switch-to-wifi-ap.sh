97d2a1eb9e4bbfb93020cc7fb272140e67c3aec7
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-06-30 20:59:48 -0700

    work

--- switch-to-wifi-ap.sh
+++ switch-to-wifi-ap.sh
@@ -1,5 +1,4 @@
 /home/pi/.config/nvm/versions/node/v8.17.0/bin/node -e "require('./scripts/switch-to-wifi-ap.js').upWifiAP()"
-sudo ip link set dev wlan0 down
 sudo ip addr flush dev wlan0
 sudo ip addr add 192.168.0.172/24 dev wlan0
 sudo systemctl restart dnsmasq.service
