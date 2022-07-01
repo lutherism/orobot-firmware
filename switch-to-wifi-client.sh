0433b790808609fb943b432a28944c4924b0bc9e
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-06-30 17:15:39 -0700

    work

--- switch-to-wifi-client.sh
+++ switch-to-wifi-client.sh
@@ -3,6 +3,6 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-/home/pi/.config/nvm/versions/node/v8.17.0/bin/node -e "require('./scripts/switch-to-wifi-client.js').createWPAConf()"
+/home/pi/.config/nvm/versions/node/v8.17.0/bin/node -e "require('./scripts/switch-to-wifi-client.js').writeWPAConf()"
 sudo systemctl stop hostapd.service
 sudo systemctl stop dnsmasq.service
