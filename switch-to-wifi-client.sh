aa33f22f75585084f98f39fcb114f0e1b3300ada
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-07-02 12:27:35 -0700

    work

--- switch-to-wifi-client.sh
+++ switch-to-wifi-client.sh
@@ -6,6 +6,6 @@ sudo killall dhcpd
 sudo killall wpa_supplicant
 sudo ifconfig wlan0 down
 sudo ifconfig wlan0 up
-sudo wpa_supplicant -i wlan0 -c/etc/wpa_supplicant/wpa_supplicant.conf
 sudo /home/pi/orobot-firmware/kill-keep-alive.sh
 sudo /home/pi/orobot-firmware/reboot.sh
+sudo wpa_supplicant -i wlan0 -c/etc/wpa_supplicant/wpa_supplicant.conf &
