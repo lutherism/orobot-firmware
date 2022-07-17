2b94b0995dce27ba47effcfa829f9fc55e95408f
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-07-16 17:18:53 -0700

    work

--- retry-ap.sh
+++ retry-ap.sh
@@ -1,5 +1,6 @@
 BASEDIR="/home/pi/orobot-firmware"
 
+echo "sudo $BASEDIR/switch-to-wifi-ap.sh &" | bash >> $BASEDIR/tmp/run.log &
 while [ "$(iw wlan0 info | grep 'type AP')" = "" ]; do
   echo "No network: $(date)"
   sleep 5
