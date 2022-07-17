6e89699aae0b526658f2713aad892acdb6502894
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-07-16 17:47:56 -0700

    work

--- retry-ap.sh
+++ retry-ap.sh
@@ -1,4 +1,5 @@
 BASEDIR="/home/pi/orobot-firmware"
+NODE_BIN="/home/pi/.config/nvm/versions/node/v8.17.0/bin/node"
 
 echo "sudo $BASEDIR/switch-to-wifi-ap.sh &" | bash >> $BASEDIR/tmp/run.log &
 while [ "$(iw wlan0 info | grep 'type AP')" = "" ]; do
@@ -6,4 +7,6 @@ while [ "$(iw wlan0 info | grep 'type AP')" = "" ]; do
   sleep 5
   echo "sudo $BASEDIR/switch-to-wifi-ap.sh &" | bash >> $BASEDIR/tmp/run.log &
 done
+echo "sudo $BASEDIR/kill-web-ap.sh" | bash
+echo "sudo $NODE_BIN $BASEDIR/ap-server.js" | bash
 echo "Updated to Access Provider mode"
