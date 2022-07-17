61240c8ecb95af467b27c8983a3abc5117c7bf68
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-07-16 17:15:44 -0700

    work

--- retry-client.sh
+++ retry-client.sh
@@ -1,5 +1,6 @@
 BASEDIR="/home/pi/orobot-firmware"
 
+echo "sudo $BASEDIR/switch-to-wifi-client.sh" | bash >> $BASEDIR/tmp/run.log &
 while [ "$(iw wlan0 info | grep 'type managed')" = "" ]; do
   echo "No network: $(date)"
   sleep 5s
