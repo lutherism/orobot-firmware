a6ed638b15a503ffb28706d4dfd60d428ab9a48d
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-07-25 15:01:40 -0700

    work

--- scripts/factory-reset.js
+++ scripts/factory-reset.js
@@ -2,6 +2,9 @@ var fs = require('fs');
 var uuid = require('uuid');
 var request = require('request');
 var childProcess = require('child_process');
+const {singleton,
+  upsertDeviceData,
+  refreshDeviceData} = require('./device-data.js');
 
 const {exec} = childProcess;
 
@@ -9,11 +12,10 @@ const DEFAULT_DEVICE_UUID = '6be50aff-6f10-4643-bfda-7d5bf15319c9';
 
 fs.readFile('./openroboticsdata/data_template.json', data => {
   const newDriverUuid = uuid.v4();
-  fs.unlinkSync(__dirname + '/openroboticsdata/data.json');
-  fs.writeFileSync(__dirname + '/openroboticsdata/data.json', JSON.stringify({
+  upsertDeviceData({
     ...data,
     deviceUuid: newDriverUuid
-  }));
+  });
   request.post('https://robots-gateway.uc.r.appspot.com/api/device', {
     json: true,
     body: {
