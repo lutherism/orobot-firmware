cee4e39c248f913017c6e74ce0ed35106d895c1e
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-07-25 18:14:30 -0700

    work

--- scripts/camera-factory-reset.js
+++ scripts/camera-factory-reset.js
@@ -2,17 +2,20 @@ var fs = require('fs');
 var uuid = require('uuid');
 var request = require('request');
 var childProcess = require('child_process');
+const {singleton,
+  upsertDeviceData,
+  refreshDeviceData} = require('./device-data.js');
 
 const {exec} = childProcess;
 
 const DEFAULT_DEVICE_UUID = '6be50aff-6f10-4643-bfda-7d5bf15319c9';
 
-fs.readFile('./openroboticsdata/camera_data_template.json', data => {
+fs.readFile('./openroboticsdata/camera_data_template.json', (err, data) => {
   const newDriverUuid = uuid.v4();
-  fs.unlinkSync(__dirname + '/openroboticsdata/data.json');
-  fs.writeFileSync(__dirname + '/openroboticsdata/data.json', JSON.stringify({
+  upsertDeviceData({
+    ...JSON.parse(data),
     deviceUuid: newDriverUuid
-  }));
+  });
   request.post('https://robots-gateway.uc.r.appspot.com/api/device', {
     json: true,
     body: {
--- scripts/factory-reset.js
+++ scripts/factory-reset.js
@@ -10,10 +10,10 @@ const {exec} = childProcess;
 
 const DEFAULT_DEVICE_UUID = '6be50aff-6f10-4643-bfda-7d5bf15319c9';
 
-fs.readFile('./openroboticsdata/data_template.json', data => {
+fs.readFile('./openroboticsdata/data_template.json', (err, data) => {
   const newDriverUuid = uuid.v4();
   upsertDeviceData({
-    ...data,
+    ...JSON.parse(data),
     deviceUuid: newDriverUuid
   });
   request.post('https://robots-gateway.uc.r.appspot.com/api/device', {
