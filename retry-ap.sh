fe41cde366a3f4b069199844c5b2f6e1f3f70db3
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-07-27 13:27:09 -0700

    work

--- retry-ap.sh
+++ retry-ap.sh
@@ -1,7 +1,6 @@
 BASEDIR="/home/pi/orobot-firmware"
 NODE_BIN="/home/pi/.config/nvm/versions/node/v8.17.0/bin/node"
 
-echo "sudo $BASEDIR/switch-to-wifi-ap.sh &" | bash >> $BASEDIR/tmp/run.log &
 while [ "$(iw wlan0 info | grep 'type AP')" = "" ]; do
   echo "No network: $(date)"
   sleep 15s
--- retry-client.sh
+++ retry-client.sh
@@ -1,6 +1,5 @@
 BASEDIR="/home/pi/orobot-firmware"
 
-echo "sudo $BASEDIR/switch-to-wifi-client.sh" | bash >> $BASEDIR/tmp/run.log &
 while [ "$(iw wlan0 info | grep 'type managed')" = "" ]; do
   echo "No network: $(date)"
   sleep 15s
