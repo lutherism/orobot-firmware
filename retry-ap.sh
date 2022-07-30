e95b6a0a9350d726198d1468a4c31c817e1417db
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-07-30 00:43:56 -0700

    work

--- retry-ap.sh
+++ retry-ap.sh
@@ -6,7 +6,10 @@ sleep 15s
 while [ "$(iw wlan0 info | grep 'type AP')" = "" ]; do
   echo "No network: $(date)"
   sleep 15s
-  echo "sudo $BASEDIR/switch-to-wifi-ap.sh &" | bash >> $BASEDIR/tmp/run.log &
+  if ps ax | grep -v grep | grep "switch-to-wifi-ap.sh" > /dev/null
+  then
+    echo "sudo $BASEDIR/switch-to-wifi-ap.sh &" | bash >> $BASEDIR/tmp/run.log &
+  fi
 done
 echo "sudo $BASEDIR/kill-web-ap.sh" | bash
 echo "sudo $NODE_BIN $BASEDIR/ap-server.js" | bash
--- retry-client.sh
+++ retry-client.sh
@@ -5,6 +5,9 @@ sleep 15s
 while [ "$(iw wlan0 info | grep 'type managed')" = "" ]; do
   echo "No network: $(date)"
   sleep 15s
-  echo "sudo $BASEDIR/switch-to-wifi-client.sh" | bash >> $BASEDIR/tmp/run.log &
+  if ps ax | grep -v grep | grep "switch-to-wifi-client.sh" > /dev/null
+  then
+    echo "sudo $BASEDIR/switch-to-wifi-client.sh" | bash >> $BASEDIR/tmp/run.log &
+  fi
 done
 echo "Update to Wifi Client mode"
