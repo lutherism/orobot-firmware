969a289a8c99b6e6e5cf27e8efc70431d3e3ef86
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-08-29 18:53:35 -0700

    work

--- scripts/keep-alive.js
+++ scripts/keep-alive.js
@@ -129,7 +129,7 @@ function recursiveConnect() {
       backoffTime = 100;
     } else {
       console.log('should retry client');
-      //exec('sudo ' + __dirname + '/../kill-switch-network.sh && sudo ' + __dirname + '/../retry-client.sh');
+      process.exit(0);
       return delay(5000).then(() => {
         backoffTime = 100;
         recursiveConnect();
