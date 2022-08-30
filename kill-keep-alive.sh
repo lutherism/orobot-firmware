32eb3eadbadbca1fdb8867d640ec4012730ebeec
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-08-29 17:20:00 -0700

    work

--- kill-keep-alive.sh
+++ kill-keep-alive.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 ps -aux | grep -v grep | grep keep-alive.js | awk '{print $2}' | xargs kill
+ps -aux | grep -v grep | grep rpi_camera_surveillance_system.py | awk '{print $2}' | xargs kill
