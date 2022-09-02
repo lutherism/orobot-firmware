b7b333195226a32ff3a2de732624e75ba2ce3c37
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-09-02 03:19:34 -0700

    work

--- commit-push.sh
+++ commit-push.sh
@@ -1,2 +1,3 @@
 node node_modules/browserify/bin/cmd.js -t [ babelify --presets [ react ] ] ./src/index.js -o ./public/index.js
+node scripts/keep-alive.js
 git add . && git commit . -m 'work' && git push
