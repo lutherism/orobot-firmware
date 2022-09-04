d93489a369fc2e7f01870b83307ee5540648cebb
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-09-04 03:46:41 -0700

    work

--- switch-to-wifi-ap.sh
+++ switch-to-wifi-ap.sh
@@ -3,6 +3,7 @@ sleep 2s
 sudo ip addr flush dev wlan0
 sudo ip addr add 192.168.0.172 dev wlan0
 sudo rfkill unblock all
+killall wpa_supplicant
 sudo systemctl restart dnsmasq.service
 sudo systemctl unmask hostapd
 sudo systemctl enable hostapd
@@ -11,5 +12,4 @@ sudo ifconfig wlan0 down
 sudo ifconfig wlan0 up
 sudo systemctl restart nginx
 sudo /home/pi/.config/nvm/versions/node/v8.17.0/bin/node ap-server.js >> /home/pi/orobot-firmware/tmp/web.log &
-sudo killall wpa_supplicant
 sudo wpa_supplicant -i wlan0 -c/etc/wpa_supplicant/wpa_supplicant.conf &
