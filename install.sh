7574f773159f55fe5b81f032cf8c62069eab1e4e
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-09-07 21:05:44 -0700

    work

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-export NVM_DIR="$HOME/.config/nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion.sh" ] && \. "$NVM_DIR/bash_completion"
+source /root/.nvm/install.sh
 nvm bash_completion
 nvm install 8
 nvm use 8
