be272466dd404931001d421b4ae7a4d680830975
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-09-07 21:11:39 -0700

    work

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-
+cp ./scripts/datatemplates/factory_install.conf /etc/wpa_supplicant/wpa_supplicant.conf
+wpa_supplicant -iwlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf &
+sleep 3
+sudo apt-get update
+printf 'y\n' | sudo apt-get install hostapd dnsmasq nginx curl
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 source /root/.nvm/install.sh
 nvm bash_completion
@@ -7,8 +11,6 @@ nvm install 8
 nvm use 8
 cd /home/pi/orobot-firmware
 npm i
-sudo apt-get update
-printf 'y\n' | sudo apt-get install hostapd dnsmasq nginx
 sudo crontab /home/pi/reboot.cron
 sudo service cron reload
 ./factory-reset.sh
