8e3b059c853266971d00f738d124fab56fac4a59
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-09-10 21:55:38 -0700

    work

--- retry-client.sh
+++ retry-client.sh
@@ -1,15 +1,16 @@
 BASEDIR="/home/pi/orobot-firmware"
 
-echo "sudo $BASEDIR/switch-to-wifi-client.sh" | bash >> $BASEDIR/tmp/run.log &
-sleep 15s
+echo "sudo $BASEDIR/switch-to-wifi-client.sh" | bash
+sleep 25s
 while [ "$(curl google.com | grep 301)" = "" ]; do
   echo "No network: $(date)"
   if ps ax | grep -v grep | grep "switch-to-wifi-client.sh" > /dev/null
   then
     echo "Already running switch to client" >> $BASEDIR/tmp/run.log
   else
-    echo "sudo $BASEDIR/switch-to-wifi-client.sh" | bash >> $BASEDIR/tmp/run.log &
+    echo "running switch";
+    echo "sudo $BASEDIR/switch-to-wifi-client.sh" | bash
   fi
-  sleep 15s
+  sleep 25s
 done
 echo "Update to Wifi Client mode"
