cbe23a6781d58ca7d3822273e8389b872f9649fb
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-09-14 21:17:22 -0700

    work

--- pages/api/wifi.js
+++ pages/api/wifi.js
@@ -1,4 +1,8 @@
 var WiFiControl = require('wifi-control');
+const {singleton,
+  upsertDeviceData,
+  refreshDeviceData} = require('./device-data.js');
+const {exec} = require('child_process');
 
 //  Initialize wifi-control package with verbose output
 WiFiControl.init({
@@ -7,10 +11,19 @@ WiFiControl.init({
 
 export default (req, res) => {
   if (req.method === 'POST') {
-    WiFiControl.connectToAP(req.body, function(err, response) {
+    upsertDeviceData({
+      wifiSettings: {
+        ssid: req.body.ssid,
+        password: req.body.password
+      }
+    });
+    exec(`sudo ${__dirname}/../../reboot.sh`, () => {
+      process.exit(0);
+    });
+    /*WiFiControl.connectToAP(req.body, function(err, response) {
       if (err) console.log(err);
       res.status(200).json(response);
-    });
+    });*/
   } else {
     WiFiControl.scanForWiFi(function(err, response) {
       if (err) console.log(err);
