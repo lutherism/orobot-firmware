1ae6cba7a53b59dbbef59cb784dc665fd118d4d1
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-09-15 19:25:20 -0700

    work

--- kill-flash-led.sh
+++ kill-flash-led.sh
@@ -1,3 +1,3 @@
-ps -aux | grep -v grep | grep flash-led.js | awk '{print $2}' | xargs kill
+ps -aux | grep -v grep | grep flash-led.js | awk '{print $2}' | xargs kill || true
 
 echo 1 | sudo tee /sys/class/leds/led0/brightness
--- kill-web-ap.sh
+++ kill-web-ap.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ps -aux | grep -v grep | grep ap-server.js | awk '{print $2}' | xargs kill
+ps -aux | grep -v grep | grep ap-server.js | awk '{print $2}' | xargs kill || true
