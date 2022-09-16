cb6915217eee4b0f6ca975a9aa720d8299a81b7a
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-09-15 20:01:39 -0700

    work

--- kill-switch-network.sh
+++ kill-switch-network.sh
@@ -1,3 +1,4 @@
+set +o pipefail
 ps -ax | grep 'switch-to' | awk '{print $1}' | xargs kill || true
 ps -ax | grep 'retry-ap.sh' | awk '{print $1}' | xargs kill || true
 ps -ax | grep 'retry-client.sh' | awk '{print $1}' | xargs kill || true
