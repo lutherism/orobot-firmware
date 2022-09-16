9b541ac0d23d54fb9e2ff1d139abe782c5006496
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-09-15 20:27:30 -0700

    work

--- switch-to-wifi-ap.sh
+++ switch-to-wifi-ap.sh
@@ -1,16 +1,12 @@
 sudo /root/.nvm/versions/node/v8.17.0/bin/node -e "require('./scripts/switch-to-wifi-ap.js').upWifiAP()"
-sleep 2s
 sudo ip addr flush dev wlan0
 sudo ip addr add 192.168.0.172 dev wlan0
 sudo rfkill unblock all
-killall wpa_supplicant
 sudo systemctl restart dnsmasq.service
 sudo systemctl stop wpa_supplicant
 sudo systemctl unmask hostapd
 sudo systemctl enable hostapd
 sudo systemctl restart hostapd.service
-sudo ifconfig wlan0 down
-sudo ifconfig wlan0 up
 sudo systemctl restart nginx
 sudo /root/.nvm/versions/node/v8.17.0/bin/node ap-server.js >> /home/pi/orobot-firmware/tmp/run.log &
-sudo wpa_supplicant -i wlan0 -c/etc/wpa_supplicant/wpa_supplicant.conf &
+sudo systemctl restart wpa_supplicant
--- switch-to-wifi-client.sh
+++ switch-to-wifi-client.sh
@@ -1,17 +1,10 @@
 sudo /root/.nvm/versions/node/v8.17.0/bin/node -e "require('/home/pi/orobot-firmware/scripts/switch-to-wifi-client.js').writeWPAConf()"
-sleep 2
 sudo systemctl stop hostapd.service
 sudo systemctl stop dnsmasq.service
 sudo rfkill unblock all
 sudo killall dhcpd
-sudo killall wpa_supplicant
-sudo killall dnsmasq
-sudo ifconfig wlan0 down
-sudo ifconfig wlan0 up
-sudo wpa_supplicant -i wlan0 -c/etc/wpa_supplicant/wpa_supplicant.conf &
-sleep 8
+sudo systemctl restart wpa_supplicant
 sudo dhclient &
 sudo systemctl restart dnsmasq
-sleep 10
 sudo /home/pi/orobot-firmware/kill-keep-alive.sh
 sudo /home/pi/orobot-firmware/reboot.sh
