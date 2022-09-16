1004c82c1b78d48b75988534d2aea683e4562f25
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-09-15 20:40:13 -0700

    work

--- switch-to-wifi-client.sh
+++ switch-to-wifi-client.sh
@@ -3,8 +3,10 @@ sudo systemctl stop hostapd.service
 sudo systemctl stop dnsmasq.service
 sudo rfkill unblock all
 sudo killall dhcpd
-sudo systemctl restart wpa_supplicant
+sudo killall wpa_supplicant
+sudo killall dnsmasq
+sudo ifconfig wlan0 down
+sudo ifconfig wlan0 up
+sudo wpa_supplicant -i wlan0 -c/etc/wpa_supplicant/wpa_supplicant.conf &
 sudo dhclient &
 sudo systemctl restart dnsmasq
-sudo /home/pi/orobot-firmware/kill-keep-alive.sh
-sudo /home/pi/orobot-firmware/reboot.sh
