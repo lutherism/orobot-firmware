f713e20eadf6733191160a9179906494cf045e1f
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-09-15 17:25:16 -0700

    work

--- kill-switch-network.sh
+++ kill-switch-network.sh
@@ -1,2 +1,2 @@
-sudo kill $(ps -ax | grep 'switch-to' | awk '{print $1}')
-sudo kill $(ps -ax | grep 'dhclient' | awk '{print $1}')
+ps -ax | grep 'switch-to' | awk '{print $1}' | xargs kill
+ps -ax | grep 'dhclient' | awk '{print $1}' | xargs kill
