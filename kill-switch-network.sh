a3f8a9104eb821a549170ec4e142d9dacd29ab3d
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-09-15 17:56:52 -0700

    work

--- kill-switch-network.sh
+++ kill-switch-network.sh
@@ -1,2 +1,4 @@
 ps -ax | grep 'switch-to' | awk '{print $1}' | xargs kill
+ps -ax | grep 'retry-ap.sh' | awk '{print $1}' | xargs kill
+ps -ax | grep 'retry-client.sh' | awk '{print $1}' | xargs kill
 ps -ax | grep 'dhclient' | awk '{print $1}' | xargs kill
