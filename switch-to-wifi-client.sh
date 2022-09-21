42d5e72b3b3d64b3441af846d1a15818db81ce47
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-09-20 19:25:29 -0700

    work

--- switch-to-wifi-client.sh
+++ switch-to-wifi-client.sh
@@ -2,12 +2,17 @@ sudo /root/.nvm/versions/node/v8.17.0/bin/node -e "require('/home/pi/orobot-firm
 sleep 2
 sudo systemctl stop hostapd.service
 sudo systemctl stop dnsmasq.service
-sudo systemctl restart wpa_supplicant
+sudo systemctl stop dhclient
+sudo systemctl stop wpa_supplicant
+sudo systemctl stop networking
 sudo rfkill unblock all
 sudo killall dhcpd
+sudo killall dhclient
+sudo killall wpa_suppplicant
 sudo killall dnsmasq
 sudo ifconfig wlan0 down
 sudo ifconfig wlan0 up
+sudo wpa_supplicant -i wlan0 -c/etc/wpa_supplicant/wpa_supplicant.conf
 sleep 3
 sudo dhclient wlan0 &
 sudo systemctl restart dnsmasq
