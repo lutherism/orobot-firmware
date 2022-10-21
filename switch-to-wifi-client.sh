d78504f99d8af30af8c1f84296bb33f0d8b83627
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-10-20 19:26:13 -0700

    work

--- switch-to-wifi-client.sh
+++ switch-to-wifi-client.sh
@@ -1,9 +1,10 @@
 sudo /root/.nvm/versions/node/v8.17.0/bin/node -e "require('/home/pi/orobot-firmware/scripts/switch-to-wifi-client.js').writeWPAConf()"
 sleep 2
+sudo systemctl stop hostapd.service
+sudo systemctl stop dnsmasq
 sudo killall dhclient
 sudo killall wpa_supplicant
-sudo ip link set dev wlan0 down
 sudo rm -rf /var/run/wpa_supplicant
-sudo systemctl stop hostapd.service
 sudo systemctl restart networking
+sudo ip link set dev wlan0 down
 sudo dhclient &
