fc02f4049f4aa847c8800d6d2a8a45d0299458d5
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-10-20 18:48:34 -0700

    work

--- scripts/switch-to-wifi-client.js
+++ scripts/switch-to-wifi-client.js
@@ -59,7 +59,7 @@ auto wlan0
 iface wlan0 inet manual
 wpa_conf /etc/wpa_supplicant/wpa_supplicant.conf
 wpa_cli log_level debug
-pre-up wpa_supplicant -iwlan0 -c/etc/wpa_supplicant/wpa_supplicant.conf -f /var/log/wpa_supplicant.log
+pre-up wpa_supplicant -B -iwlan0 -c/etc/wpa_supplicant/wpa_supplicant.conf -f /var/log/wpa_supplicant.log
 post-down wpa_cli -i wlan0 terminate
 `
 }
