6c3fe4884a81051c7484146ad9fe0a65b5689259
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-10-20 20:02:58 -0700

    work

--- scripts/switch-to-wifi-client.js
+++ scripts/switch-to-wifi-client.js
@@ -15,7 +15,6 @@ const networkingPath = "/etc/network/interfaces";
 const createWPAConf = () => {
   if (singleton.DeviceData.wifiSettings.username) {
     return `country=US
-ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
  update_config=1
  ap_scan=1
  freq_list=2412 2417 2422 2427 2432 2437 2442 2447 2452 2457 2462 2467 2472
@@ -35,7 +34,6 @@ network={
   `;
   }
   return `country=US
-ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
 update_config=1
 freq_list=2412 2417 2422 2427 2432 2437 2442 2447 2452 2457 2462 2467 2472
 
